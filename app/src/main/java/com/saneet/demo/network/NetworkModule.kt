e885163d6b9162d3a6169bd1a54138e5a65bf2ef
Saneet <saneet@Yousras-MacBook-Pro.local>
2022-12-06 21:34:45 -0600

    Added leakcanary and logging for Retrofit

--- app/src/main/java/com/saneet/demo/network/NetworkModule.kt
+++ app/src/main/java/com/saneet/demo/network/NetworkModule.kt
@@ -3,6 +3,8 @@ package com.saneet.demo.network
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -11,9 +13,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 class NetworkModule {
     @Provides
     fun provideCoroutineService(): APIService.CoroutineService {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         val gson = GsonBuilder().create()
         val retrofit = Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
         return retrofit.create(APIService.CoroutineService::class.java)
@@ -21,9 +30,16 @@ class NetworkModule {
 
     @Provides
     fun provideRxService(): APIService.RxService {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         val gson = GsonBuilder().create()
         val retrofit = Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
+            .client(client)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create(gson)).build()
         return retrofit.create(APIService.RxService::class.java)
