63bdc0f353036c9fadc3f48c097e5e4173292aeb
Saneet <saneet@Yousras-MacBook-Pro.local>
2023-02-10 04:11:24 -0800

    Working version

--- app/src/main/java/com/saneet/demo/feature/FeatureFragment.kt
+++ app/src/main/java/com/saneet/demo/feature/FeatureFragment.kt
@@ -1,13 +1,18 @@
 package com.saneet.demo.feature
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.saneet.demo.DemoApplication
+import com.saneet.demo.PickSinglePhotoContract
 import com.saneet.demo.R
 import com.saneet.demo.ViewModelFactory
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class FeatureFragment : Fragment() {
@@ -25,6 +30,16 @@ class FeatureFragment : Fragment() {
         )
     }
 
+    private val singlePhotoPickerLauncher =
+        registerForActivityResult(PickSinglePhotoContract()) { imageUri: Uri? ->
+            imageUri?.let { viewModel.setImageUri(requireContext(), it) }
+        }
+
+
+    var asked: Boolean = false
+
+    private fun pickPhoto() = singlePhotoPickerLauncher.launch(null)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,5 +50,31 @@ class FeatureFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         (requireActivity().application as DemoApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        lifecycleScope.launch {
+            viewModel.viewState.collect {
+                setImage(it)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!asked) {
+            pickPhoto()
+            asked = true
+        }
+
+    }
+
+    private fun setImage(viewState: FeatureViewModel.ViewState) {
+        viewState.imageBitmap?.let {
+            requireView().findViewById<ImageView>(R.id.image)
+                .setImageBitmap(it)
+        }
     }
 }
--- app/src/main/java/com/saneet/demo/feature/FeatureViewModel.kt
+++ app/src/main/java/com/saneet/demo/feature/FeatureViewModel.kt
@@ -1,8 +1,37 @@
 package com.saneet.demo.feature
 
+import android.content.ContentResolver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.io.InputStream
 import javax.inject.Inject
 
 class FeatureViewModel @Inject constructor() : ViewModel() {
+    data class ViewState(
+        val imageBitmap: Bitmap? = null,
+    )
 
+    private val _viewState = MutableStateFlow(ViewState())
+    val viewState = _viewState.asStateFlow()
+    fun setImageUri(context: Context, uri: Uri) {
+        viewModelScope.launch(Dispatchers.IO) {
+            context.contentResolver.let { contentResolver: ContentResolver ->
+                val readUriPermission: Int = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                contentResolver.takePersistableUriPermission(uri, readUriPermission)
+                contentResolver.openInputStream(uri)?.use { inputStream: InputStream ->
+                    _viewState.emit(ViewState(imageBitmap = BitmapFactory.decodeStream(inputStream)))
+                }
+            }
+        }
+    }
 }
+
